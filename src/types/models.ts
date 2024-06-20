4ec7d2cf93cb775fd19824717b324b61b1b5ed80
Harsingh Sekhon <harsinghsekhon007@gmail.com>
2024-06-20 14:23:48 -0600

    Did a lot of stuff

---
+++ src/types/models.ts
@@ -0,0 +1,12 @@
+export interface ModelTransform {
+    translateX: number;
+    translateY: number;
+    translateZ: number;
+    rotateX: number;
+    rotateY: number;
+    rotateZ: number;
+    scale: number;
+  }
+export interface model {
+  
+}
