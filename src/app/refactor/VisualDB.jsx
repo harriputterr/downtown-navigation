587c64d96466d6a676aea7117b64dfefaf77957b
souravnub <s6hr6h4u3@gmail.com>
2024-07-30 14:18:18 -0600

    fixed tailwind config

--- src/app/refactor/VisualDB.jsx
+++ src/app/refactor/VisualDB.jsx
@@ -12,63 +12,62 @@ import { addDataNode } from "./components/DataNodeCRUD.jsx";
 import { loadCustomLayerAndEventListeners } from "./components/PostMapLoad.jsx";
 
 export default function VisualDB({ setNodeStateObj, tb, setTb, map, setMap }) {
-  const [pickables, setPickables] = useState([]);
-  const mapRef = useRef(null);
+    const [pickables, setPickables] = useState([]);
+    const mapRef = useRef(null);
 
-  useEffect(() => {
-    // Creating the mapbox map instance.
-    if (mapRef.current) {
-      setMap(createMap(mapRef));
-    }
-  }, []);
+    useEffect(() => {
+        // Creating the mapbox map instance.
+        if (mapRef.current) {
+            setMap(createMap(mapRef));
+        }
+    }, []);
 
-  // useEffect(() => {
-  //   loadData();
-  // }, []);
+    // useEffect(() => {
+    //   loadData();
+    // }, []);
 
-  useEffect(() => {
-    if (map) {
-      // Creating the Threebox Instance
-      setTb(createThreeboxInstance(map));
-    }
-  }, [map]);
+    useEffect(() => {
+        if (map) {
+            // Creating the Threebox Instance
+            setTb(createThreeboxInstance(map));
+        }
+    }, [map]);
 
-  useEffect(() => {
-    if (tb) {
-      // Attaching the threebox instance to the global window object to make it available globally.
-      window.tb = tb;
-      // Instantiate a Stats object.
-      let stats = new Stats();
+    useEffect(() => {
+        if (tb) {
+            // Attaching the threebox instance to the global window object to make it available globally.
+            window.tb = tb;
+            // Instantiate a Stats object.
+            let stats = new Stats();
 
-      // Creating an animate function to which will be called recursively when the map loads.
-      const animate = () => {
-        requestAnimationFrame(animate);
-        stats.update();
-      };
+            // Creating an animate function to which will be called recursively when the map loads.
+            const animate = () => {
+                requestAnimationFrame(animate);
+                stats.update();
+            };
 
-      // Loading our Customer Layer with all of the Map events
-      loadCustomLayerAndEventListeners(
-        map,
-        tb,
-        stats,
-        animate,
-        createCustomLayer,
-        addDataNode,
-        modelData,
-        setPickables,
-        setNodeStateObj
-      );
+            // Loading our Customer Layer with all of the Map events
+            loadCustomLayerAndEventListeners(
+                map,
+                tb,
+                stats,
+                animate,
+                createCustomLayer,
+                addDataNode,
+                modelData,
+                setPickables,
+                setNodeStateObj
+            );
 
-      return () => {
-        map.remove();
-      };
-    }
-  }, [tb]);
+            return () => {
+                map.remove();
+            };
+        }
+    }, [tb]);
 
-  return (
-    <>
-        <div ref={mapRef}  className="w-screen h-screen" />
-
-    </>
-  );
+    return (
+        <>
+            <div ref={mapRef} className="w-screen h-screen" />
+        </>
+    );
 }
