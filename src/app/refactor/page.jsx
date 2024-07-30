1c1aa5667fa0fb00b145423510718bcd9ce24030
souravnub <s6hr6h4u3@gmail.com>
2024-07-30 16:40:49 -0600

    navbar for mobile devices

--- src/app/refactor/page.jsx
+++ src/app/refactor/page.jsx
@@ -6,6 +6,18 @@ import CreateRelations from "./components/CreateRelations";
 import DeleteRelationSheet from "./components/DeleteRelationViaUUID";
 import ModelSettings from "./components/ModelSettings";
 
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
 export default function page() {
     const [tb, setTb] = useState(null);
     const [map, setMap] = useState(null);
@@ -21,7 +33,7 @@ export default function page() {
     return (
         <>
             <div>
-                <nav className="flex gap-5  fixed py-2 container  bg-black/50 top-0 inset-x-0 z-50">
+                <nav className=" gap-5  fixed py-2 container  bg-black/50 top-0 inset-x-0 z-50 hidden md:flex">
                     <DeleteRelationSheet />
                     <ModelSettings map={map} tb={tb} />
                     <CreateRelations
@@ -38,6 +50,41 @@ export default function page() {
                         />
                     )}
                 </nav>
+
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button
+                            variant="outline"
+                            className="md:hidden fixed right-5 top-5 z-50">
+                            Open Menu
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent className="md:hidden">
+                        <SheetHeader>
+                            <SheetTitle>Menu</SheetTitle>
+                            <SheetDescription></SheetDescription>
+                        </SheetHeader>
+
+                        <nav className=" flex flex-col gap-5 py-2 ">
+                            <DeleteRelationSheet />
+                            <ModelSettings map={map} tb={tb} />
+                            <CreateRelations
+                                nodeStateObj={nodeStateObj}
+                                tb={tb}
+                                map={map}
+                            />
+                            {nodeStateObj.selectedNode && (
+                                <NodeEditSheet
+                                    className={"ml-auto"}
+                                    nodeStateObj={nodeStateObj}
+                                    map={map}
+                                    tb={tb}
+                                />
+                            )}
+                        </nav>
+                    </SheetContent>
+                </Sheet>
+
                 <VisualDB
                     setNodeStateObj={setNodeStateObj}
                     tb={tb}
