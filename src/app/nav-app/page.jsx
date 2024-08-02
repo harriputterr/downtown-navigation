e8d65261a715c3161eab9718ec502882eeea7e66
Harsingh Sekhon <harsinghsekhon007@gmail.com>
2024-08-02 17:21:50 -0600

    Create Camera View Switch Button

--- src/app/nav-app/page.jsx
+++ src/app/nav-app/page.jsx
@@ -10,46 +10,73 @@ import ImageDisplay from "./components/ImageDisplay";
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 export default function Page() {
-    const mapboxRef = useRef(null);
-    const [nodes, setNodes] = useState([]);
-    const [from, setFrom] = useState(null);
-    const [to, setTo] = useState(null);
+  const mapboxRef = useRef(null);
+  const [nodes, setNodes] = useState([]);
+  const [from, setFrom] = useState(null);
+  const [to, setTo] = useState(null);
 
-    useEffect(() => {
-        if (mapboxRef.current) {
-            const map = new mapboxgl.Map({
-                container: mapboxRef.current,
-                style: "mapbox://styles/mapbox/light-v9",
-                center: [-114.063775, 51.0475053],
-                zoom: 16,
-                antialias: true,
-            });
+  useEffect(() => {
+    if (from && to){
+        console.log("From and To are now selected.")
+    }
+  }, [from, to])
+  
+  useEffect(() => {
+    if (mapboxRef.current) {
+      const map = new mapboxgl.Map({
+        container: mapboxRef.current,
+        style: "mapbox://styles/mapbox/light-v9",
+        center: [-114.063775, 51.0475053],
+        zoom: 16,
+        antialias: true,
+      });
 
-            const geolocate = new mapboxgl.GeolocateControl({
-                positionOptions: {
-                    enableHighAccuracy: true,
-                },
-                showAccuracyCircle: false,
-                trackUserLocation: true,
-                //   showUserHeading: true,
-            });
+      const marker = new mapboxgl.Marker()
+        .setLngLat([-114.063775, 51.0475053])
+        .addTo(map);
 
-            getAllNodes().then((res) => {
-                const data = res.data.map((ele) => ele.n.properties);
-                setNodes(data);
-            });
+      const geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        showAccuracyCircle: false,
+        trackUserLocation: true,
+        //   showUserHeading: true,
+      });
 
-            map.addControl(geolocate);
+      getAllNodes().then((res) => {
+        const data = res.data.map((ele) => ele.n.properties);
+        setNodes(data);
+      });
 
-            return () => {
-                map.remove(); // Clean up on unmount
-            };
-        }
-    }, []); // Empty dependency array ensures this effect runs only once
+      map.addControl(geolocate);
 
-    return (
-        <div className="w-96 h-96">
-            <ImageDisplay />
-        </div>
-    );
+      return () => {
+        map.remove(); // Clean up on unmount
+      };
+    }
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  return (
+    <div className="w-screen h-screen">
+      <div ref={mapboxRef} className="w-screen h-screen"></div>;
+      <div className="flex flex-col gap-2 absolute top-5 right-5 min-w-[15rem]">
+        <SearchBox
+          placeholder="From"
+          elements={nodes}
+          onSelectChange={(val) => {
+            setFrom(val);
+          }}
+        />
+        <SearchBox
+          placeholder="To"
+          elements={nodes}
+          onSelectChange={(val) => {
+            setTo(val);
+          }}
+        />
+      </div>
+      {/* <ImageDisplay /> */}
+    </div>
+  );
 }
